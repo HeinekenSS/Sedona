f0745eebdb4d7ae2e8e5eda69ae31b234ab74a2e
HeinekenSS <heineken2202@gmail.com>
2018-01-31 00:50:02 +0300

    -
    -

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var link = document.querySelector('.btn-search');
 var modal = document.querySelector('.modal');
-var close = document.querySelector('.btn-search');
+var Close = document.querySelector('.btn-search');
 link.addEventListener('click', function (event) {
     event.preventDefault();
     modal.classList.toggle('open')
